fbd155dc660b3c8167791c945ac988e3d0879fea
asadulR <asad1252001@gmail.com.com>
2022-04-29 16:39:17 +0600

    database connected to the server

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 4000;
 
 
@@ -12,11 +14,18 @@ app.get('/', (req, res) => {
     res.send('Auto Accord server');
 });
 
+//     AutoAccord
+//    65TvtWdj2UfVbdov
 
 
-
-
-
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f3est.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log("DB connected");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 
